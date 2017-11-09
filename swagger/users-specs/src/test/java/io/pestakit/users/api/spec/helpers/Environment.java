3f57bfa3ff9b8530779dcb30118135510aac4f2b
McMoudi <mcmoudi@gmail.com>
2017-11-09 15:11:01 +0100

    users-specs init

---
+++ swagger/users-specs/src/test/java/io/pestakit/users/api/spec/helpers/Environment.java
@@ -0,0 +1,28 @@
+package io.pestakit.users.api.spec.helpers;
+
+import io.pestakit.users.api.DefaultApi;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Olivier Liechti on 24/06/17.
+ */
+public class Environment {
+
+    private DefaultApi api = new DefaultApi();
+
+    public Environment() throws IOException {
+        Properties properties = new Properties();
+        properties.load(this.getClass().getClassLoader().getResourceAsStream("environment.properties"));
+        String url = properties.getProperty("io.avalia.fruits.server.url");
+        //api.getApiClient().setBasePath(url);
+
+    }
+
+    public DefaultApi getApi() {
+        return api;
+    }
+
+
+}
