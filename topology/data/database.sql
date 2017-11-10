0bdb5c0fd028803d12afe05296033bfecd7dd5f3
Andrea Cotza <andrea@cotza.me>
2017-11-10 10:01:15 +0100

    DB - Create user table

--- topology/data/database.sql
+++ topology/data/database.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE "users-service";
 
+USE "users-service";
 
+CREATE TABLE "user" (
+        id INT NOT NULL DEFAULT unique_rowid(),
+        username STRING NOT NULL,
+        firstname STRING,
+        lastname STRING,
+        email STRING NOT NULL,
+        password STRING NOT NULL,
+        CONSTRAINT "primary" PRIMARY KEY (id ASC),
+        UNIQUE INDEX user_username_key (username ASC),
+        UNIQUE INDEX user_email_key (email ASC),
+        FAMILY "primary" (id, username, firstname, lastname, email, password)
+);
