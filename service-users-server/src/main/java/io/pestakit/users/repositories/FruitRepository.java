f8ef9b8b853c82f97bb16c38017a4de9075f0500
McMoudi <mcmoudi@gmail.com>
2017-11-15 21:08:36 +0100

    user created #6

--- service-users-server/src/main/java/io/pestakit/users/repositories/FruitRepository.java
+++ service-users-server/src/main/java/io/pestakit/users/repositories/FruitRepository.java
@@ -3,9 +3,10 @@ package io.pestakit.users.repositories;
 import io.pestakit.users.entities.FruitEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Olivier Liechti on 26/07/17.
  */
 public interface FruitRepository extends CrudRepository<FruitEntity, Long>{
-
 }
