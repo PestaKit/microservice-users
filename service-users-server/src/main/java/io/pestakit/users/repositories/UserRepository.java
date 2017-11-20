a5e2ffa13109f91eb4099836bce4c439e68b0044
Lou <luana.martelli@heig-vd.ch>
2017-11-20 10:53:22 +0100

    Add method findByUsernameIgnoreCase()

--- service-users-server/src/main/java/io/pestakit/users/repositories/UserRepository.java
+++ service-users-server/src/main/java/io/pestakit/users/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import io.pestakit.users.entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByUsernameIgnoreCase(String username);
 }
